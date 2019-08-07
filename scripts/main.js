47432a06c81c3af377752a111ad6d261bd74e58e
Evan-Kim2028 <ekcopersonal@gmail.com>
2019-08-07 19:17:44 -0400

    Added new tabs with pictures

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-let myImage = document.querySelector('img');
+let myImage = document.querySelector('h1');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
@@ -15,20 +15,3 @@ document.querySelector('h1').onclick = function() {
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
-  }
-
-if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
-  }
-
-myButton.onclick = function() {
-    setUserName();
-  }
